5350c8caf5dcc863e55c9f4ccc6e95405a26ceaf
Gabriel Wottawa <gabrielclerio10@gmail.com>
2022-11-23 00:22:33 -0300

    criação de entities

--- SimulationEngine/Restaurant/Entities/ClientGroup.cs
+++ SimulationEngine/Restaurant/Entities/ClientGroup.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SimulationEngine.Api.Models;
+using SimulationEngine.Api.Models.Interfaces;
 
 namespace Restaurant.Entities
 {
-    public class ClientGroup
+    public class ClientGroup : ManagedEntity
     {
+        public int Qty { get; }
+
+        public Order Order { get; set; }
+
+        public IEnumerable<IManagedAllocation<Resource>> OccupiedPlace;
+
+        private readonly Random random = new Random();
+
+        public ClientGroup()
+        {
+            Qty = random.Next(1, 5);
+        }
     }
 }
--- SimulationEngine/Restaurant/Entities/Order.cs
+++ SimulationEngine/Restaurant/Entities/Order.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SimulationEngine.Api.Models;
 
 namespace Restaurant.Entities
 {
-    internal class Order
+    public class Order : ManagedEntity
     {
+        private ClientGroup clientGroup { get; set; }
+
+        public bool ReadyToEat { get; set; }
+
+        public Order(ClientGroup clientGroup)
+        {
+            this.clientGroup = clientGroup;
+        }
     }
 }
